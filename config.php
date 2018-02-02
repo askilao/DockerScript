8c0bf52d81e8755e988c005e3b7916c9e1f27ef4
Adrian <36070441+amojamo@users.noreply.github.com>
2018-02-02 10:59:08 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$dbhost = "10.10.0.200";
+$dbport = "3306";
+$db = "bookface";
+$dbuser = "superuser";
+$dbpassw = 'greatpw';
+$webhost = '10.212.136.60';
+$weburl = 'http://' . $webhost ;
+?>
