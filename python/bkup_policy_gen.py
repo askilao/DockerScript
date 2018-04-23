af08e88fe4ed0e02342ec568d39aaa92aaf3804f
Adrian <36070441+amojamo@users.noreply.github.com>
2018-04-23 17:31:29 +0200

    fixd + comments

--- python/bkup_policy_gen.py
+++ python/bkup_policy_gen.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+#Generates a .conf file listing relevant machines that needs to have a backup.
+
 import os
 import argparse
 import subprocess
@@ -7,10 +9,10 @@ import subprocess
 VERBOSE = 0
 DEBUG = 0
 
-BACKUP_CONFIG="adrian_policy.conf"
+BACKUP_CONFIG="backup_policy.conf"
 EXCLUDED='nobackup' # tag on server that should be excluded from backup
 
-parser = argparse.ArgumentParser(prog='adrian_hosts.py')
+parser = argparse.ArgumentParser(prog='backup_hosts.py')
 parser.add_argument('-v','--verbose',dest="verbose",help='Turn verbosity on',default=False,action="store_true")
 parser.add_argument('-d','--debug',dest="debug",help='Turn debug_messages on',default=False,action="store_true")
 parser.add_argument('-e','--excluded',dest="excluded",help='Tag for filtering out what not to back up',metavar="STRING",default=EXCLUDED)
@@ -33,10 +35,9 @@ verbose("Getting active IPs...")
 serverlist = subprocess.check_output(["openstack server list --status active | grep -v " + EXCLUDED + " | grep imt3003 |awk '{print $8}' | sed -e 's/.*=//;s/,//'"],shell=True)
 serverlist = serverlist.rstrip()
 serverlist = serverlist.split('\n')
-# 2 check if elegible servers are already in backup policy
 
 try:
-    os.remove("adrian_policy.conf")
+    os.remove("backup_policy.conf")
 except OSError:
     pass
 
@@ -45,7 +46,5 @@ backupfile = open(BACKUP_CONFIG, "a+")
 for ip in serverlist:
     verbose(str(ip))
     backupfile.write(str(ip) + ":/etc,/home/ubuntu\n")
-    
 
 backupfile.close()
-# 3 add the ones who is not already added to policy file
